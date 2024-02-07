5a8f706fe729f6cd48eca80e080ef98c7280033d
Moggieuk <moggieuk@gmail.com>
2024-02-08 02:34:00 +0700

    print version number in installer

--- happy_hare/install_ks.sh
+++ happy_hare/install_ks.sh
@@ -67,12 +67,15 @@ self_update() {
         git pull --quiet --force
         git checkout $BRANCH --quiet
         git pull --quiet --force
+	GIT_VER=$(git describe --tags)
+        echo -e "${B_GREEN}Now on git version {GIT_VER}"
         echo -e "${B_GREEN}Running the new install script..."
         cd - >/dev/null
         exec "$SCRIPTNAME" "${ARGS[@]}"
         exit 1 # Exit this old instance
     }
-    echo -e "${B_GREEN}Already the latest version."
+    GIT_VER=$(git describe --tags)
+    echo -e "${B_GREEN}Already the latest version: {GIT_VER}"
 }
 
 function nextsuffix {
